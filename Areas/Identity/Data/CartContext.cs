a460c2dadf0ad500387e1ab0765ff8675ecd803f
Nis-08 <123791314+Nis-08@users.noreply.github.com>
2023-04-13 20:27:58 +0530

    Add files via upload
    added login

---
+++ Areas/Identity/Data/CartContext.cs
@@ -0,0 +1,32 @@
+﻿using Cart.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Cart.Data;
+
+public class CartContext : IdentityDbContext<CartUser>
+{
+    public CartContext(DbContextOptions<CartContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<CartUser>
+{
+    public void Configure(EntityTypeBuilder<CartUser> builder)
+    {
+        builder.Property(x=>x.Name).IsRequired();
+        builder.Property(x=>x.PhoneNo).IsRequired();
+    }
+}
